7009880d15effc5e73dcd02ebf1452f49e4a2519
Tejas Thakare <tthakare73@gmail.com>
2023-09-07 15:13:40 +0000

    Added Graphs

---
+++ src/components/GraphCompoentRegistory.jsx
@@ -0,0 +1,8 @@
+import { BarChart } from './Graph/BarChart';
+import Line from './Graph/Line';
+
+export const GraphCompoentRegistory = {
+  line_chart: Line,
+  bar_chart: BarChart,
+  // Add more table components as needed
+};
--- src/components/MainComponentRegistory.jsx
+++ src/components/MainComponentRegistory.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { tableRegistry } from './TableComponentRegistory';
+import { GraphCompoentRegistory } from "./GraphCompoentRegistory"
 
 const ComponentRegistry = {
   table: tableRegistry,
-//   graph: Graph,
+  graph: GraphCompoentRegistory,
 //   custom1: CustomComponent1,
 //   custom2: CustomComponent2,
   // Add more custom components as needed
