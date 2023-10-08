14a3b2382f14b5d05760bbe56ac261a698356ab0
Tejas Thakare <tthakare73@gmail.com>
2023-10-08 04:38:21 +0000

    Added compoennt render as service

--- src/App.jsx
+++ src/App.jsx
@@ -4,85 +4,52 @@ import "./mockapis"
 import './App.css'
 import { ComponentRegistry } from './components/MainComponentRegistory'
 function App() {
-  const [count, setCount] = useState(0)
-  const [ response, setResponse ] = useState([])
-  
-  useEffect(()=> {
-    axios.get("/config").then((res) => {
-      setResponse(res.data.config)
-    })
-  }, [])
+  const [service, setService ] = useState([]);
 
-  function generateRandomData(columns, numRows) {
-    const data = [];
-  
-    for (let i = 0; i < numRows; i++) {
-      const row = {};
-      columns.forEach((column) => {
-        const dataType = column.dataType;
-        const columnName = column.name;
-        let value;
-  
-        switch (dataType) {
-          case 'string':
-            value = generateRandomString();
-            break;
-          case 'number':
-            value = generateRandomNumber();
-            break;
-          case 'boolean':
-            value = generateRandomBoolean();
-            break;
-          // Add more data types as needed
-          default:
-            value = null;
-        }
-  
-        row[columnName] = value;
-      });
-  
-      data.push(row);
+  useEffect(()=> {
+    if(service.length === 0) {
+      axios.get("/config").then((res) => {
+        setService([...service, res.data.config])
+      })
     }
-  
-    return data;
-  }
-  
-  function generateRandomString() {
-    return Math.random().toString(36).substring(2, 15);
-  }
-  
-  function generateRandomNumber() {
-    return Math.floor(Math.random() * 1000);
-  }
-  
-  function generateRandomBoolean() {
-    return Math.random() < 0.5;
-  }
+  }, [])
 
   return (
     <>
     <h2> Fooo Yoo.... 🎉</h2>
-    <div style={{display: "flex", flexWrap: "wrap"}}>
-      {response.map((item, index) => {
-        
-          const Table = ComponentRegistry[item['type']][item['tableType']]
+      {
+        service.map((val, index)=> {
           return (
-            <div style={item['tableProperties']}>
-            <Table
-              key={index}
-              api={item.api}
-              tableType={item.tableType}
-              name={item.name}
-              tableProperties={item.tableProperties}
-              columns={item.columns}
-              data={item.data}
-            />
+            <div style={{ marginTop: "5rem" }}>
+              <div style={{display: "flex", flexWrap: "wrap"}}>
+              <div onClick={()=> {
+                              axios.get("/config_2").then((res) => {
+                                setService((pre)=> ([...pre, res.data.config]))
+                              })
+                            }}>{index}</div>
+                {
+                  val.map((item, i) => {
+          
+                    const Table = ComponentRegistry[item['type']][item['tableType']]
+                    return (
+                      <div style={item['tableProperties']}>
+                      <Table
+                        key={i}
+                        api={item.api}
+                        tableType={item.tableType}
+                        name={item.name}
+                        tableProperties={item.tableProperties}
+                        columns={item.columns}
+                        data={item.data}
+                      />
+                      </div>
+                    )})
+                }
+              </div>
             </div>
-          );
-        
-      
-      })}
-    </div>
+          )
+        })
+      }
     </>
   )
 }
